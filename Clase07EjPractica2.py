fd8a170673fb6450ef3445784ccbee206e698910
GhostieSpoomy <valeri.silvam@duocuc.cl>
2025-04-25 11:25:20 -0400

    Update Clase07EjPractica2.py

--- Clase07EjPractica2.py
+++ Clase07EjPractica2.py
@@ -1,7 +1,16 @@
+
 # EJERCICIO TIPO EXAMEN - CLASE 07:
 
 
-# Solicitar dos números (a y b) a un usuario.
-# "a" debe ser el límite inferior y "b" el límite superior, para que el resultado sea un número aleatorio.
-# Sacar un porcentaje del resultado según la respuesta del usuario.
-# Solicitar al usuario que sume o reste el porcentaje que colocó a su número aleatorio.
+# La academia requiere realizar una operación que les permita establecer el rango socioeconómico de cada estudiante, para poder proporcionar los distintos beneficios.
+# Sabiendo que cada uno de los deciles establecidos cuenta con una diferencia del 10%.
+
+# El decil n1 beneficia al estrato económico más bajo va a recibir una beca total con calificaciones de 7.0.
+# El decil n2 beneficia al nivel bajo con calificaciones de 7.0, estableciendo un 40%.
+# El decil n3 beneficia al grupo medio bajo, recibiendo un 30% con puntaje 7.0.
+# El decil n4 beneficia al grupo amedio, recibiendo un 30% con puntaje de 6.0.
+# El decil n5 beneficia al grupo medio alto, recibiendo un 25% con puntaje 6.0.
+# El decil n6 beneficia al alto, recibiendo un 20%, con puntaje 5.0
+# El decil n7 que pertenece al grupo élite, no recibe beneficio.
+
+# El precio del curso mensual es de $300.000.
