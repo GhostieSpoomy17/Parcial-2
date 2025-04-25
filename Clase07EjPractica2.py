d85d23e94ebb973462a7fd80e04b5f00b4ea8ac2
GhostieSpoomy <valeri.silvam@duocuc.cl>
2025-04-25 11:24:59 -0400

    Update Clase07EjPractica2.py

---
+++ Clase07EjPractica2.py
@@ -0,0 +1,7 @@
+# EJERCICIO TIPO EXAMEN - CLASE 07:
+
+
+# Solicitar dos números (a y b) a un usuario.
+# "a" debe ser el límite inferior y "b" el límite superior, para que el resultado sea un número aleatorio.
+# Sacar un porcentaje del resultado según la respuesta del usuario.
+# Solicitar al usuario que sume o reste el porcentaje que colocó a su número aleatorio.
