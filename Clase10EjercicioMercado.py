f4fa60b343394bad3d2f157a11441a0146f065e7
GhostieSpoomy <valeri.silvam@duocuc.cl>
2025-05-05 10:47:21 -0400

    Update Clase10EjercicioMercado.py

--- Clase10EjercicioMercado.py
+++ Clase10EjercicioMercado.py
@@ -5,5 +5,5 @@
 # Dentro de las mermas vamos a considerar la pérdida del 25% del pollo.
 # Si el día miércoles recibimos 1.000 kg de pollo, el día jueves recibimos 1.200 kg de pollo y el sábado solo 500 kg. Considerar la merma de esa semana.
 # Realizar una lista de 10 productos que solo se permitirá regresar 3, que no deben sobrepasar el 20% total de todos los productos.
-3 Mostrar en pantalla los productos que cumplen el 20%, seguido de la merma de la semana.
+# Mostrar en pantalla los productos que cumplen el 20%, seguido de la merma de la semana.
 # Dicho esto, debe dar la opción de salir del programa.
